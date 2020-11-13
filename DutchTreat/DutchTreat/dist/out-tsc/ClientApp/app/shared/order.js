9755af07d5a24d3ee35568eb6ab0097e918e776c
Pedro Nilo <pedroniloz.nicola@gmail.com>
2020-11-13 20:10:40 -0300

    remove dutch-app and dist directories from root of project

--- DutchTreat/DutchTreat/dist/out-tsc/ClientApp/app/shared/order.js
+++
@@ -1,9 +0,0 @@
-export class Order {
-    constructor() {
-        this.orderDate = new Date();
-        this.items = new Array();
-    }
-}
-export class OrderItem {
-}
-//# sourceMappingURL=order.js.map
--- DutchTreat/DutchTreat/dist/out-tsc/wwwroot/ts/main.js
+++
@@ -1,3 +0,0 @@
-let shopper = new StoreCustomer('Pedro Nilo', 'Zonderico Nicola');
-shopper.showName();
-//# sourceMappingURL=main.js.map
